bdd5c81bb3e35c1b9a5a55b2ca3edd9c74da2ba7
JackyChuang <LAPTOP-6IR4E4AF\Tibame_T14>
2021-06-08 15:02:08 +0800

    2nd Version

---
+++ gitText/src/gitText/packge.java
@@ -0,0 +1,10 @@
+package gitText;
+
+public class packge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("2nd Version");
+	}
+
+}
---
+++ gitText/src/gitText/text2.java
@@ -0,0 +1,5 @@
+package gitText;
+
+public class text2 {
+
+}
